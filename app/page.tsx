2d71a1fb8bc448021ad498332775976cb02b89a9
naelshichida <nael.shichida2@gmail.com>
2024-02-10 12:20:39 +0000

    Updated onchain  dependency and aspect ratio

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import { NEXT_PUBLIC_BASE_URL,NEXT_PUBLIC_GATEWAY_URL} from './config';
-
+import React from 'react';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
@@ -9,7 +9,10 @@ const frameMetadata = getFrameMetadata({
       label: "Read the tales of VERA",
     }
   ],
-  image: `${NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmcQVMUmP3jxXwEG3uzMUGAXgp9dSn27fdn47dxypE1Wjg/0.png`,
+  image: {
+    src: `${NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmcQVMUmP3jxXwEG3uzMUGAXgp9dSn27fdn47dxypE1Wjg/0.png`,
+    aspectRatio: '1:1',
+},
   post_url: `${NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
